7e7109d1357f2f39fa01d5977f338056de13c323
Prerna <prerngup@adobe.com>
2020-05-06 19:21:46 +0530

    Added functionality for movie and genre search

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Main from './Main'
+import Background from './images/topmovies.jpg'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+var sectionStyle = {
+  width: "100%",
+  height: "400px",
+  backgroundImage: `url(${Background})`
+};
+
+class App extends Component {
+
+  render() {
+
+    return (
+      <section style={ sectionStyle }>
+        <Main />
+      </section>
+    );
+  }
+  
 }
+
 
 export default App;
